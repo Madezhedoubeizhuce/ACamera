2300c1d88ab80901977b6bda753daedd428c259d
Madezhedoubeizhuce <Madezhedoubeizhuce@163.com>
2021-06-01 00:26:11 +0800

    test

--- app/src/androidTest/java/com/alpha/acamera/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/alpha/acamera/ExampleInstrumentedTest.java
@@ -23,5 +23,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.alpha.acamera", appContext.getPackageName());
+		// test
     }
 }
